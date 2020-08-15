5480c38b9d60e87a283b8abe12e4467e2dd18956
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-08-15 10:17:48 -0500

    promesas ejemplo2

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-promesas',
@@ -7,29 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromesasComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
 
-    const promesa = new Promise((resolve, reject)=>{
+    //   const promesa = new Promise((resolve, reject)=>{
+    //
+    //     if(!true){
+    //       resolve("Hola desde resolve");
+    //     }else{
+    //       reject("Algo salio mal");
+    //     }
+    //   });
+    //
+    //   promesa.then((mensajeResolve)=>{
+    //     console.log(mensajeResolve);
+    //   }).catch(()=>{  // para manejar el error que genera el Reject
+    //     console.log("manejado el error");
+    //   });
+    //   console.log("Fin del init");
+    //
 
-      if(!true){
-        resolve("Hola desde resolve");
-      }else{
-        reject("Algo salio mal");
-      }
+//-----------1 y 2a forma-----------
+//     this.getUsuarios();
+
+//-----------3a forma-----------
+    this.getUsuarios()
+      .then(usuarios=>{
+        console.log(usuarios);
+      });
+
+  }//fin init
+
+  getUsuarios() {
+
+    //-----3a forma-------------
+    return new Promise(resolve => {
+      fetch('https://reqres.in/api/users')
+        .then(resp => resp.json())
+        .then(body => console.log(body.data));
     });
 
-    promesa.then((mensajeResolve)=>{
-      console.log(mensajeResolve);
-    }).catch(()=>{  // para manejar el error que genera el Reject
-      console.log("manejado el error");
-    });
-    console.log("Fin del init");
+    //-- forma 1 y 2
+    // fetch('https://reqres.in/api/users')
+    //-----------1a forma-----------
+    // .then(respuesta =>{
+    //   respuesta.json().then(body=>console.log(body));
+    // });
+    //-----------2a forma-----------
+    // .then(resp=>resp.json())
+    // .then(body=>console.log(body.data));
+
 
   }
-
-
 
 
 }
