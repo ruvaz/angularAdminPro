b904f3eb452d8390433f7cf10c71d170dff1ec32
Ruben Vazquez <ruvaz@live.com>
2020-09-16 17:40:41 -0500

    google sign in working

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, Validators} from "@angular/forms";
 import {UsuarioService} from "../../services/usuario.service";
 import {LoginForm} from "../../interfaces/login-form.interface";
 import Swal from "sweetalert2";
+import {environment} from "../../../environments/environment";
 
 
 //deja de marcar gapi aqui eerror y lo busca en el index
@@ -17,6 +18,9 @@ declare const gapi: any;
 export class LoginComponent implements OnInit {
 
   public formSubmitted = false;
+
+  // va a contener toda la autenticacion
+  public auth2: any;
 
   public loginForm = this.fb.group({
     email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
@@ -56,15 +60,8 @@ export class LoginComponent implements OnInit {
 
   //* ** Googlesign in
 
-  onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token);
-  }
+  // var id_token = googleUser.getAuthResponse().id_token;
 
-  onFailure(error) {
-    console.log(error);
-  }
 
   renderButton() {
     gapi.signin2.render('my-signin2', {
@@ -73,10 +70,39 @@ export class LoginComponent implements OnInit {
       'height': 50,
       'longtitle': true,
       'theme': 'dark',
-      'onsuccess': this.onSuccess,
-      'onfailure': this.onFailure
     });
+
+    this.startApp();
   }
+
+
+  startApp() {
+    gapi.load('auth2',  () => {
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      this.auth2 = gapi.auth2.init({
+        client_id: environment.GOOGLE_ID,
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      this.attachSignin(document.getElementById('my-signin2'));
+    });
+  };
+
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+      // login con exito  se tiene token
+        const id_token = googleUser.getAuthResponse().id_token;
+        console.log(id_token);
+        //dispara la autenticacion
+        this.usuarioService.loginGoogle(id_token);
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
 
   ngOnInit(): void {
     this.renderButton();
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -42,4 +42,15 @@ export class UsuarioService {
       );
   }
 
+
+  loginGoogle(token) {
+    return this.http.post(`${base_url}/login/google`, {token})
+      .pipe(
+        tap( (resp:any)=> {
+          console.log(resp )
+          localStorage.setItem('token',resp.token)
+        })
+      );
+  }
+
 }
