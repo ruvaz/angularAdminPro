fb0c46c854eb9c180c8f6973866e6d8847c35b61
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-09-29 17:29:23 -0500

    Fin seccion 16

---
+++ src/app/components/modal-imagen/modal-imagen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-imagen',
+  templateUrl: './modal-imagen.component.html',
+  styleUrls: ['./modal-imagen.component.css']
+})
+export class ModalImagenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
